0c73b01545c7563a293aee1006e00e8de2075f21
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-05 11:34:05 +0100

    add new page from admin

--- admin/index.php
+++ admin/index.php
@@ -1,27 +1,52 @@
 <?php
 $db = new mysqli("localhost", "root", "", "cms");
 
-if($_REQUEST['action'] == "pageList") {
+if ($_REQUEST['action'] == "pageList") {
     $q = $db->prepare("SELECT * FROM page");
     $q->execute();
     $result = $q->get_result();
     echo '<table>';
-    foreach($result as $row) {
+    foreach ($result as $row) {
         echo '<tr>';
-        echo '<td>'.$row['id'].'</td>';
-        echo '<td>'.$row['title'].'</td>';
-        echo '<td><a href="index.php?action=editPage&pageID='.$row['id'].'">
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['title'] . '</td>';
+        echo '<td><a href="index.php?action=editPage&pageID=' . $row['id'] . '">
                 <button>Edytuj</button></a></td>';
         echo '</tr>';
     }
     echo '<table>';
+    echo '<a href="index.php?action=newPage">Nowa strona</a>';
+}
+
+if ($_REQUEST['action'] == "newPage") {
+?>
+    <form action="index.php" method="post">
+        <label for="titeInput">Tytuł strony:</label>
+        <input type="text" name="title" id="titleInput" value=""> <br>
+        <label for="contentTextArea">Treść strony:</label>
+        <textarea name="content" id="contentTextArea" cols="30" rows="10">
+    </textarea>
+        <input type="hidden" name="action" value="savePage">
+        <input type="hidden" name="pageID" value="0">
+        <input type="submit" value="Zapisz">
+    </form>
+    <a href="index.php?action=pageList">Powrót</a>
+<?php
 }
 
 if ($_REQUEST['action'] == "savePage" && $_REQUEST['pageID'] != null) {
-    $q = $db->prepare("UPDATE page SET title = ?, content = ? WHERE id = ?");
-    $q->bind_param("ssi", $_REQUEST['title'], $_REQUEST['content'], $_REQUEST['pageID']);
-    $q->execute();
-    echo "Strona zapisana poprawnie!";
+    if ($_REQUEST['pageID'] == 0) {
+        //nowa strona
+        $q = $db->prepare("INSERT INTO page VALUES (NULL, ?, ?)");
+        $q->bind_param("ss", $_REQUEST['title'], $_REQUEST['content']);
+        $q->execute();
+        echo "Strona utworzona poprawnie!";
+    } else {
+        $q = $db->prepare("UPDATE page SET title = ?, content = ? WHERE id = ?");
+        $q->bind_param("ssi", $_REQUEST['title'], $_REQUEST['content'], $_REQUEST['pageID']);
+        $q->execute();
+        echo "Strona zapisana poprawnie!";
+    }
 }
 if ($_REQUEST['action'] == "editPage" && $_REQUEST['pageID'] != null) {
 
