1a6e6f9a4bd312e7b482ab840e2c7be54331ccf9
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-06 10:00:03 +0100

    baic routing for frontend

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require('./smarty/Smarty.class.php');
+require('./Route.php');
+
+use Steampixel\Route; 
 
 $s = new Smarty();
 
@@ -10,30 +13,57 @@ $s->setConfigDir('./smarty/configs');
 
 $db = new mysqli("localhost", "root", "", "cms");
 
+Route::add('/', function() {
+    global $s;
+    $s->assign('navMenu', getNavMenu());
+    $s->assign('page', getPage(1));
+    $s->display('index.tpl');
+});
 
-$q = $db->prepare("SELECT id, title FROM page");
-$q->execute();
-$result = $q->get_result();
-$navMenu = array();
-foreach($result as $row) {
-    //$pageID = $row['id'];
-    //$title = $row['title'];
-    //echo '<a href="index.php?pageID='.$pageID.'">'.$title.'</a>';
-    array_push($navMenu, $row);
+Route::add('/strona/([0-9]*)', function($i) {
+    global $s;
+    $s->assign('navMenu', getNavMenu());
+    $s->assign('page', getPage($i));
+    $s->display('index.tpl');
+});
+//ręcznie
+Route::add('/kontakt-recznie', function() {
+    global $s;
+    $s->assign('navMenu', getNavMenu());
+    $s->assign('page', getPage(6));
+    $s->display('index.tpl');
+});
+
+
+// Run the router
+Route::run('/cms');
+
+function getPage(int $pageId) : array {
+    global $db;
+    $q = $db->prepare("SELECT * FROM page WHERE id = ? LIMIT 1");
+    $q->bind_param("i", $pageId);
+    $q->execute();
+    $result = $q->get_result();
+    
+    //tablica asocjacyjna z pojedyńczą stroną
+    return $result->fetch_assoc();
 }
-$s->assign('navMenu', $navMenu);
+function getNavMenu() : array {
+    global $db;
+    $q = $db->prepare("SELECT id, title FROM page");
+    $q->execute();
+    $result = $q->get_result();
+    $navMenu = array();
+    foreach($result as $row) {
+        //$pageID = $row['id'];
+        //$title = $row['title'];
+        //echo '<a href="index.php?pageID='.$pageID.'">'.$title.'</a>';
+        array_push($navMenu, $row);
+    }
+    return $navMenu;
+}
 
 
-$pageID = intval($_REQUEST['pageID']);
-$q = $db->prepare("SELECT * FROM page WHERE id = ? LIMIT 1");
-$q->bind_param("i", $pageID);
-$q->execute();
-$result = $q->get_result();
 
-//tablica asocjacyjna z pojedyńczą stroną
-$page = $result->fetch_assoc();
 
-$s->assign('page', $page);
-
-$s->display('index.tpl');
 ?>
